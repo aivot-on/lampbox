f270a76e5bd052a2cd127a63c29e9e97f487ed9b
Henri Niemeläinen <aivot-on@iki.fi>
2015-12-12 15:50:28 +0200

    Use file provisioning for php7 conf

--- Vagrantfile
+++ Vagrantfile
@@ -8,11 +8,14 @@ Vagrant.configure(2) do |config|
   config.vm.network "forwarded_port", guest: 22, host: 6840
   config.vm.network "private_network", ip: "10.200.42.30"
   
-  config.vm.synced_folder "../data", "/vagrant_default"
+  config.vm.synced_folder "./data", "/data"
+  # config.vm.synced_folder "./data", "/data", type: "nfs"
   
   config.vm.provider "virtualbox" do |vb|
-    vb.memory = "2048"
+    vb.cpus = 2
+    vb.memory = 2048
   end
   
+  config.vm.provision "file", source: "provision/php7-fpm.conf", destination: "provision/php7-fpm.conf"
   config.vm.provision :shell, path: "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,83 +1,27 @@
-# Clone php-src and install required software
-sudo apt-get update
-sudo apt-get -y install git vim
-git clone https://github.com/php/php-src.git --depth=1
-cd php-src
-sudo apt-get -y install build-essential autoconf automake libtool bison re2c
-sudo apt-get -y install libxml2-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libpng12-dev libc-client-dev libmcrypt-dev libxslt1-dev
-sudo ln -s /usr/lib/libc-client.a /usr/lib/x86_64-linux-gnu/libc-client.a
-
-# Build PHP
-./buildconf
-./configure --enable-fpm \
---with-libdir=/lib/x86_64-linux-gnu \
---with-kerberos \
---with-fpm-user=vagrant \
---with-fpm-group=vagrant \
---without-pear \
---with-openssl \
---with-pdo-sqlite \
---with-pdo-mysql=mysqlnd \
---enable-sockets \
---with-gettext \
---with-curl \
---enable-exif \
---enable-ftp \
---with-gd \
---enable-mbstring \
---with-mcrypt \
---enable-zip \
---with-zlib \
---with-imap \
---with-imap-ssl \
---enable-xmlreader \
---enable-phar \
---with-xsl \
---enable-cgi \
---with-jpeg-dir=/usr \
---with-png-dir=/usr
-make
-sudo make install
-
-# Setting up PHP-FPM
-sudo cp sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
-sudo chmod 755 /etc/init.d/php-fpm
-sudo cp /usr/local/etc/php-fpm.conf.default /usr/local/etc/php-fpm.conf
-sudo cp /usr/local/etc/php-fpm.d/www.conf.default /usr/local/etc/php-fpm.d/www.conf
-
-sudo sed -i '$ d' /usr/local/etc/php-fpm.conf
-echo 'include=etc/php-fpm.d/*.conf' | sudo tee -a /usr/local/etc/php-fpm.conf
-
-sudo service php-fpm start
-
-# Start php-fpm on startup
-sudo update-rc.d php-fpm defaults
-
-# Setting up Apache
-# Not quite commenting out but it works:
-echo -e 'deb http://archive.ubuntu.com/ubuntu trusty multiverse \n' | sudo tee -a /etc/apt/sources.list
-echo -e 'deb-src http://archive.ubuntu.com/ubuntu trusty multiverse \n' | sudo tee -a /etc/apt/sources.list
-echo -e 'deb http://archive.ubuntu.com/ubuntu trusty-updates multiverse \n' | sudo tee -a /etc/apt/sources.list
-echo -e 'deb-src http://archive.ubuntu.com/ubuntu trusty-updates multiverse \n' | sudo tee -a /etc/apt/sources.list 
-
-sudo apt-get update
-sudo apt-get -y install apache2-mpm-worker libapache2-mod-fastcgi
-sudo a2enmod actions fastcgi alias
-
-echo -e '<IfModule mod_fastcgi.c>\n' | sudo tee -a /etc/apache2/apache2.conf
-echo -e '        AddHandler php5-fcgi .php\n' | sudo tee -a /etc/apache2/apache2.conf
-echo -e '        Action php5-fcgi /php5-fcgi\n' | sudo tee -a /etc/apache2/apache2.conf
-echo -e '        Alias /php5-fcgi /usr/lib/cgi-bin/php5-fcgi\n' | sudo tee -a /etc/apache2/apache2.conf
-echo -e '        FastCgiExternalServer /usr/lib/cgi-bin/php5-fcgi -host 127.0.0.1:9000 -pass-header Authorization\n' | sudo tee -a /etc/apache2/apache2.conf
-echo -e '</IfModule>\n' | sudo tee -a /etc/apache2/apache2.conf
-
-sudo sed -ie '/<Directory \/>/{N; N; N; N; d;}' /etc/apache2/apache2.conf
-
-sudo service apache2 restart
-
-rm /var/www/html/index.html
-echo '<?php phpinfo();' > /var/www/html/index.php
-
 sudo debconf-set-selections <<< 'mariadb-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mariadb-server mysql-server/root_password_again password root'
-sudo apt-get install -y mariadb-server
+
+echo 'Adding ppa:ondrej/php-7.0 to repositories'
+sudo apt-get update
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository ppa:ondrej/php-7.0
+
+echo 'Updating repositories'
+sudo apt-get update
+sudo apt-get install -y git vim
+sudo apt-get install -y mariadb-server apache2 apache2-mpm-worker libapache2-mod-fastcgi php7.0 php7.0-common php7.0-fpm php7.0-mysql
+
+echo 'Configuring apache2 and php7'
+sudo cp provision/php7-fpm.conf /etc/apache2/conf-available/php7-fpm.conf
+
+sudo a2dismod mpm_prefork
+sudo a2enmod actions fastcgi alias mpm_worker
+sudo a2enconf php7-fpm
+
+sudo service apache2 restart
+sudo service php7.0-fpm restart
+
+sudo chown -R vagrant:www-data /var/www/
+echo '<?php phpinfo();' > /var/www/html/info.php
+
+echo 'If you dont see a sea of red above, you are probably good to go.'
